7e441961fb3308a0c2be09455c38d3a899d8452b
lucbazzani <lu.bazzani@hotmail.com>
2021-11-23 12:25:24 -0300

    H2 databse and automatic database creation

--- src/main/java/com/lucbazzani/cursomc/domain/Categoria.java
+++ src/main/java/com/lucbazzani/cursomc/domain/Categoria.java
@@ -1,11 +1,19 @@
 package com.lucbazzani.cursomc.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
 public class Categoria implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
     private String name;
 
     public Categoria() {
