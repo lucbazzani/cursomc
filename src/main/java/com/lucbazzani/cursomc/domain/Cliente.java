69804cf468225113f9ee709412bddc374fcfc552
lucbazzani <lu.bazzani@hotmail.com>
2021-11-26 17:38:41 -0300

    Pedido, EstadoPagamento and Pagamento

--- src/main/java/com/lucbazzani/cursomc/domain/Cliente.java
+++ src/main/java/com/lucbazzani/cursomc/domain/Cliente.java
@@ -26,6 +26,9 @@ public class Cliente implements Serializable {
     @CollectionTable(name = "TELEFONE")
     private Set<String> telefones = new HashSet<>();
 
+    @OneToMany(mappedBy = "cliente")
+    private List<Pedido> pedidos = new ArrayList<>();
+
     public Cliente() {
     }
 
@@ -83,6 +86,10 @@ public class Cliente implements Serializable {
 
     public Set<String> getTelefones() {
         return telefones;
+    }
+
+    public List<Pedido> getPedidos() {
+        return pedidos;
     }
 
     @Override
